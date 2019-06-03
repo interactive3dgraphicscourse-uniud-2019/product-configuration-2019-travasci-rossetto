8fd5212f1fcd884709d45ffa6a0c49f9307695a8
StefanoTrv <stefanotravasci@gmail.com>
2019-06-04 00:48:42 +0200

    Integrata la scena nella pagina web La scena che era stata creata nel file main.html è stata inserita nella pagina web del sito fittizio

---
+++ resources/renderScene.js
@@ -0,0 +1,69 @@
+			var scene, renderer, camera, stats;
+			
+
+			function Start() {
+
+				scene = new THREE.Scene();
+				camera = new THREE.PerspectiveCamera( 75, 915 / 500, 0.1, 1000 );
+				renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true} );
+				var controls = new THREE.OrbitControls( camera, document.getElementById("canvas") );
+				var glasses = new THREE.Mesh();
+
+				renderer.setSize( 915, 500 );
+				renderer.setClearColor( 0xf0f0f0 );
+				camera.position.set( 0, 1, 30 );
+
+				vs = document.getElementById("vertex").textContent;
+				fs = document.getElementById("fragment").textContent;
+
+				var uniforms = {
+					cdiff:	{ type: "v3", value: new THREE.Vector3( 0.7, 0.0, 0.0 ) },
+					pointLightPosition:	{ type: "v3", value: new THREE.Vector3() },
+					clight:	{ type: "v3", value: new THREE.Vector3( 1.0, 1.0, 1.0 ) },
+					cspec:	{ type: "v3", value: new THREE.Vector3( 0.04, 0.04, 0.3 ) },
+					roughness:	{ type: "f", value: 0.5 },
+				};
+
+				// instantiate a loader
+				var loader = new THREE.OBJLoader();
+				loader.load(
+					"../models/glasses/Glasses_v2.0.obj", 
+					function( object ) {
+						
+						//console.log(object);  // debug
+
+						glasses = object;
+						glasses.traverse( function (child) {
+							if( child instanceof THREE.Mesh ) {
+								child.geometry.computeVertexNormals();
+								// apply this material to all the meshes in the OBJ
+								child.material = new THREE.ShaderMaterial({uniforms: uniforms, vertexShader: vs, fragmentShader: fs});	
+							};
+						});
+						glasses.position.set(0, 1, 10);
+						glasses.scale.multiplyScalar(10);			// They're very little(?)
+						scene.add(glasses);
+						renderer.render( scene, camera );
+					}
+				);
+
+				var lightMesh = new THREE.Mesh( new THREE.SphereGeometry(1, 16, 16), new THREE.MeshBasicMaterial ({color: 0xffff00, wireframe:true}));
+				lightMesh.position.set( 30.0, 15.0, 15.0 );
+				uniforms.pointLightPosition.value = new THREE.Vector3(lightMesh.position.x,
+				lightMesh.position.y,
+				lightMesh.position.z);
+
+				scene.add(lightMesh);
+
+				/*stats = new Stats();
+				stats.domElement.style.position = 'absolute';
+				stats.domElement.style.top = '0px';
+				document.body.appendChild( stats.domElement );*/
+
+			}
+
+			function Update() {
+				requestAnimationFrame(Update);
+				//stats.update();
+				renderer.render(scene, camera);
+			}
